3735c833e85d1d2730824b25a07e27ce0219acd7
鄧紹偉 <sdonline13@gmail.com>
2024-04-05 11:45:23 +0800

    add csrf ignore api

--- src/main/java/com/kucw/security/security/MySecurityConfig.java
+++ src/main/java/com/kucw/security/security/MySecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -37,14 +38,20 @@ public class MySecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
+                // 設定 Session 的創建機制
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                // 設定CSRF 保護 , 當前端發post put  delete 時 需檢查 XSRF-TOKEN
                 .csrf(csrf ->// 表示在 cookie 中回傳 XSRF-TOKEN 給前端
                         csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                                 .csrfTokenRequestHandler(csrfTokenRequestAttributeHandler())
-                )
+                                // 忽略特定api
+                                .ignoringRequestMatchers("/register","/userLogin")
+                 )
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
 
                 .authorizeHttpRequests(request -> request
+                        .requestMatchers("/register","/userLogin").permitAll()
                         .anyRequest().authenticated()
                 )
 
